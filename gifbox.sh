dc4516cae82a2691c16f892d38c1f8f0cfc59657
prehensile <me@prehensile.co.uk>
2015-02-07 13:09:23 +0000

    added splash, install & runner script

---
+++ gifbox.sh
@@ -0,0 +1,20 @@
+case "$1" in
+  start)
+    # selfupdate
+    git pull
+    # activate virtualenv
+    source .venv/bin/activate
+    # start main & save pid
+    sudo python gifbox.py &
+    echo $! >~/.gifbox/pid
+    exit 0
+    ;;
+  stop)
+    sudo kill -TERM $(cat ~/.gifbox/pid)
+    exit 0
+    ;;
+  *)
+    echo "Usage: /etc/init.d/gifbox {start|stop}"
+    exit 1
+    ;;
+esac
---
+++ install.sh
@@ -0,0 +1,16 @@
+# install dependencies
+sudo apt-get install gifsicle fbi python-pip -y
+
+# create & provision a virtualenv
+sudo pip install virtualenv
+virtualenv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
+# install splash screen 
+sudo ln -s init.d/asplash.sh /etc/init.d/asplash.sh 
+sudo update-rc.d asplash.sh defaults
+
+# link init.d script
+sudo ln -s init.d/gifbox.sh /etc/init.d/gifbox.sh
+sudo update-rc.d gifbox.sh defaults
